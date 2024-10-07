ff7e84fcb4f959d63c552e24664acb3b26d6e2dc
ritahecht <rita.hecht@outlook.com>
2024-10-07 14:12:06 -0300

    first commit

---
+++ src/main/java/ads/fatec/AvaliacaoIT002/AvaliacaoIt002Application.java
@@ -0,0 +1,13 @@
+package ads.fatec.AvaliacaoIT002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoIt002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoIt002Application.class, args);
+	}
+
+}
---
+++ src/test/java/ads/fatec/AvaliacaoIT002/AvaliacaoIt002ApplicationTests.java
@@ -0,0 +1,13 @@
+package ads.fatec.AvaliacaoIT002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoIt002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
